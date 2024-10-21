dfcc291301896fcbc6748a0a126aff0fa134ed14
Roma <rasevychr@gmail.com>
2024-10-21 13:35:25 +0300

    sol

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,12 @@
-export const UserInfo = () => <>Put the user here</>;
+import React from 'react';
+
+export const UserInfo = ({ user }) => {
+  return (
+    <div className="UserInfo">
+      <p className="UserInfo__name">{user.name}</p>
+      <a className="UserInfo__email" href={`malito:${user.email}`}>
+        {user.email}
+      </a>
+    </div>
+  );
+};
